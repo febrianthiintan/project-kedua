5d47aafe4033b0ef4043b493c429d6fe88c16d7a
febrianthiintan <44692349+febrianthiintan@users.noreply.github.com>
2018-11-02 12:58:19 +0700

    Add files via upload

---
+++ project-kedua/project-kedua/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from 'E:/KULIAH/Semester 3/Pemrograman Web Lanjut/Pertemuan-8/project-kedua/src/app/shared/ingredient.model' ;
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit {
+
+  //ViewChild dekorator digunakan untuk mendapatkan akses ke komponen anak,
+  // yang ditemukan di tamplate, sehingga Anda dapat mengakses properti dan metodenya
+  @ViewChild('nameInput') nameRef:ElementRef;
+  @ViewChild('amountInput') amountRef:ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddItem(){
+    const ingName = this.nameRef.nativeElement.value;
+    const ingAmount = this.amountRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
+}
